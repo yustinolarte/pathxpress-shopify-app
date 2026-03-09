e06de75666669e95f28beb81fd2df05807d61e6c
yustin olarte perez <yustinolarte@gmail.com>
2026-03-10 02:38:00 +0400

    fix: remove invalid company field and use url in ReverseDeliveryTrackingInput

--- server.js
+++ server.js
@@ -964,7 +964,7 @@ app.post(
                         reverseDeliveryId: deliveryId,
                         trackingInput: {
                             number: returnWaybill,
-                            company: "PathXpress"
+                            url: `https://pathxpress.ae/track/${returnWaybill}`
                         }
                     });
 
